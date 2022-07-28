94e5862767a1872a675ea96023ae4eb644bbe32f
galenomoon <gui.galenocover@gmail.com>
2022-07-28 16:04:00 -0300

    QRCodesData Update + Modal Refactored

--- components/MessageModal/index.jsx
+++ components/MessageModal/index.jsx
@@ -8,17 +8,21 @@ const MessageModal = ({ modalVisible, closeModal, qrCodeData }) => {
     <Modal animationType="slide" transparent={true} visible={modalVisible}>
       <View style={tw`flex-1 justify-center items-center`}>
         <View style={tw`w-[100%] h-[100%] justify-center items-center rounded-2xl`}>
-          <TO style={tw`z-53 absolute top-31 right-7`} onPress={() => closeModal()}>
-            <Icon name="close" style={tw` z-53 m-1`} color={'#333'} size={40} />
-          </TO>
-          <View style={tw`bg-white w-[90%] h-[73%] rounded-lg overflow-hidden p-2`}>
-            <View style={tw`flex-5 overflow-hidden`}>
-              <Image style={tw`w-full h-full rounded-xl`} source={{ uri: qrCodeData?.img_url }} />
+          <View style={tw`bg-white w-[98%] h-[80%] items-center justify-center rounded-lg overflow-hidden p-2`}>
+            <View style={tw`flex-1 items-end justify-between w-full`}>
+              <TO style={tw`z-53`} onPress={() => closeModal()}>
+                <Icon name="close" style={tw`m-1`} color={'#333'} size={40} />
+              </TO>
             </View>
-            <Text style={tw`text-2xl font-bold m-2`}>{qrCodeData?.title}</Text>
-            <ScrollView style={tw`flex-4 mx-2`}>
-              <Text style={tw`text-xl`}>{qrCodeData?.message}</Text>
-            </ScrollView>
+            <View style={tw`flex-10 justify-center w-full`}>
+              <View style={tw`flex-5 overflow-hidden`}>
+                <Image style={tw`w-full h-full rounded-xl`} source={{ uri: qrCodeData?.img_url }} />
+              </View>
+              <Text style={tw`text-2xl font-bold m-2`}>{qrCodeData?.title}</Text>
+              <ScrollView style={tw`flex-4 mx-2`}>
+                <Text style={tw`text-xl`}>{qrCodeData?.message}</Text>
+              </ScrollView>
+            </View>
           </View>
         </View>
       </View>
--- utils/qrCodeModel.js
+++ utils/qrCodeModel.js
@@ -2,20 +2,20 @@ let modelList =
   [
     {
       "id": 1,
-      "title": "Oi oii gatinha",
-      "message": "Finalmente chegou o dia 21/08, mas você ainda não, de 130km digamos que agora estamos a uns 11.5km um do outro, mas calma, o agora é só o começo, vai naquela vendinha, mostra essa foto pra moça da vendinha, de resto bem, você vai saber o que fazer, te amo <3",
+      "title": "Oi oii gatinha ❤️",
+      "message": "Finalmente chegou o dia 21/08, mas você ainda não, de 130km digamos que agora estamos à uns 11.5km um do outro, mas calma, é só o começo, agora vai naquela vendinha que eu sempre te compro doce aí no terminal e mostra essa foto pra moça da vendinha, de resto bem, você vai saber o que fazer, te amo <3",
       "img_url": "https://drogariavenancio.vteximg.com.br/arquivos/ids/767983-1000-1000/51984_Z.jpg?v=637341100558830000"
     },
     {
       "id": 2,
-      "title": "",
-      "message": "",
-      "img_url": "https://drogariavenancio.vteximg.com.br/arquivos/ids/767983-1000-1000/51984_Z.jpg?v=637341100558830000"
+      "title": "Espero que tenha gostado do docinho 🤭",
+      "message": "Agora a frase mais clichê que eu poderia pensar \"Vá para o lugar onde nos vimos pela primeira vez\", kkkk. Relaxa, não vou te fazer andar sem rumo pela PG kkk, clica nesse botãozinho aqui que você já vai ser redirecionada pro uber com o endereço certinho, quem sabe lá você encontre o último balão, te amo <3",
+      "img_url": "https://instagram.fssz3-1.fna.fbcdn.net/v/t51.2885-15/240481315_533268347766971_580683814768857931_n.jpg?stp=dst-jpg_e35&cb=2d435ae8-d7f9aae8&_nc_ht=instagram.fssz3-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=9iZgBVyRE-MAX8MJWp3&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MjY0NjkzMDcyNDQwMTMyNTgwNg%3D%3D.2-ccb7-5&oh=00_AT9czoyI2NjoWhc5ADwEdYvjqj_HXQjl3Yn8MhpkSHRAsQ&oe=62E71F6D&_nc_sid=30a2ef"
     },
     {
       "id": 3,
-      "title": "teste",
-      "message": "aaa",
-      "img_url": "https://instagram.fssz3-1.fna.fbcdn.net/v/t51.2885-15/240481315_533268347766971_580683814768857931_n.jpg?stp=dst-jpg_e35&cb=2d435ae8-d7f9aae8&_nc_ht=instagram.fssz3-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=9iZgBVyRE-MAX8MJWp3&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MjY0NjkzMDcyNDQwMTMyNTgwNg%3D%3D.2-ccb7-5&oh=00_AT9czoyI2NjoWhc5ADwEdYvjqj_HXQjl3Yn8MhpkSHRAsQ&oe=62E71F6D&_nc_sid=30a2ef"
-    }
+      "title": "Tadaaaa 🎉",
+      "message": "Bem gatinha, espero que tenha gostado de tudo isso, e não esquenta a cabeça não, é só o começo, eu quero fazer da sua vida feliz, quero que se sinta numa realidade em que as coisas são mais leves do que realmente são e que por mais que cheguem dificuldades, de alguma forma ainda assim, aqui compense isso tudo, eu te amo e espero que goste da música, não é um \"Apollo 11\" da vida mas acho que serve kkkk, te amo, obrigado por tudo <3",
+      "img_url": "https://drogariavenancio.vteximg.com.br/arquivos/ids/767983-1000-1000/51984_Z.jpg?v=637341100558830000"
+    },
   ]
